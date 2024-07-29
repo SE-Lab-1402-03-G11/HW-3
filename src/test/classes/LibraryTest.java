f13d2de535d8ba46161582fee0e11e1a8948582b
mahdi <mahdi13812002@gmail.com>
2024-07-29 12:48:56 +0330

    add searchStudents and handle_duplicate tests

--- src/test/classes/LibraryTest.java
+++ src/test/classes/LibraryTest.java
@@ -2,6 +2,7 @@ package test.classes;
 
 import static org.junit.Assert.assertEquals;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import org.junit.Test;
@@ -12,10 +13,103 @@ public class LibraryTest {
 
 	@Test
 	public void testSearchStudents_return_null() throws Exception{
+		Student student1 = new Student("John Doe", 1);
+		Student student2 = new Student("Mahdi Q", 2);
+		Library libray = new Library();
+		libray.addStudent(student1);
+		libray.addStudent(student2);
 
+		ArrayList<Object> key_name = new ArrayList<Object>();
+		key_name.add("John Do");
 
+		ArrayList<Object> key_id = new ArrayList<Object>();
+		key_id.add(3);
+
+		ArrayList<Student> students = libray.searchStudents(SearchByType.NAME, key_name);
+		assertEquals(null, students);
+
+		students = libray.searchStudents(SearchByType.ID, key_id);
+		assertEquals(null, students);
 	}
 	
+	@Test
+	public void testSearchStudents_search_by_name() throws Exception{
+		Student student1 = new Student("John Doe", 1);
+		Student student2 = new Student("Mahdi Q", 2);
+		Library libray = new Library();
+		libray.addStudent(student1);
+		libray.addStudent(student2);
+
+		ArrayList<Object> key_name = new ArrayList<Object>();
+		key_name.add("John Doe");
+
+		ArrayList<Student> students = libray.searchStudents(SearchByType.NAME, key_name);
+		assertEquals(1, students.size());
+		assertEquals(student1, students.get(0));
+
+		key_name.add("Mahdi Q");
+		students = libray.searchStudents(SearchByType.NAME, key_name);
+		assertEquals(2, students.size());
+
+		key_name.add("Ali");
+		students = libray.searchStudents(SearchByType.NAME, key_name);
+		assertEquals(2, students.size());
+	}
+
+	@Test
+	public void testSearchStudents_search_by_id() throws Exception{
+		Student student1 = new Student("John Doe", 1);
+		Student student2 = new Student("Mahdi Q", 2);
+		Library libray = new Library();
+		libray.addStudent(student1);
+		libray.addStudent(student2);
+
+		ArrayList<Object> key_id = new ArrayList<Object>();
+		key_id.add(1);
+
+		ArrayList<Student> students = libray.searchStudents(SearchByType.ID, key_id);
+		assertEquals(1, students.size());
+		assertEquals(student1, students.get(0));
+
+		key_id.add(2);
+		students = libray.searchStudents(SearchByType.ID, key_id);
+		assertEquals(2, students.size());
+
+		key_id.add(3);
+		students = libray.searchStudents(SearchByType.ID, key_id);
+		assertEquals(2, students.size());
+	}
+
+	@Test
+	public void testSearchStudents_handle_name_duplicates() throws Exception{
+		Student student1 = new Student("John Doe", 1);
+		Student student2 = new Student("John Doe", 2);
+		Library libray = new Library();
+		libray.addStudent(student1);
+		libray.addStudent(student2);
+
+		ArrayList<Object> key_name = new ArrayList<Object>();
+		key_name.add("John Doe");
+
+		ArrayList<Student> students = libray.searchStudents(SearchByType.NAME, key_name);
+		assertEquals(2, students.size());
+	}
+
+	@Test
+	public void testSearchStudents_hanle_id_duplicate() throws Exception{
+		Student student1 = new Student("John Doe", 1);
+		Student student2 = new Student("Mahdi Q", 1);
+		Library libray = new Library();
+		libray.addStudent(student1);
+		libray.addStudent(student2);
+
+		ArrayList<Object> key_id = new ArrayList<Object>();
+		key_id.add(1);
+
+		ArrayList<Student> students = libray.searchStudents(SearchByType.ID, key_id);
+		assertEquals(2, students.size());
+	}
+
 	@Test
 	public void testSearchBooks_return_null1() {
 		Library libray = new Library();
@@ -119,6 +213,77 @@ public class LibraryTest {
         libray.addBook(book2);
 		key.add(2);
 		books = libray.searchBooks(SearchByType.ID, key);
+		assertEquals(2, books.size());
+	
+
+		libray.lendBook(book1, student);
+		books = libray.searchBooks(SearchByType.ID, key);
+		assertEquals(1, books.size());
+		assertEquals(book2, books.get(0));	
+	}
+
+	@Test
+	public void testSearchBooks_handle_title_duplicate() {
+		Library libray = new Library();
+		Student student = new Student("John Doe", 1);
+		libray.addStudent(student);
+		Book book1 = new Book("Harry Potter: The Philosopher's Stone", "J.K. Rowling", 1);
+        libray.addBook(book1);
+
+		ArrayList<Object> key = new ArrayList<Object>();
+		key.add("Harry Potter: The Philosopher's Stone");
+
+
+		Book book2 =  new Book("Harry Potter: The Philosopher's Stone", "J.K. Rowling", 2);
+        libray.addBook(book2);
+		ArrayList<Book> books = libray.searchBooks(SearchByType.TITLE, key);
+		assertEquals(2, books.size());
+	
+
+		libray.lendBook(book1, student);
+		books = libray.searchBooks(SearchByType.TITLE, key);
+		assertEquals(1, books.size());
+		assertEquals(book2, books.get(0));	
+	}
+
+	@Test
+	public void testSearchBooks_handle_duplicate_author() {
+		Library libray = new Library();
+		Student student = new Student("John Doe", 1);
+		libray.addStudent(student);
+		Book book1 = new Book("Harry Potter: The Philosopher's Stone", "J.K. Rowling", 1);
+        libray.addBook(book1);
+
+		ArrayList<Object> key = new ArrayList<Object>();
+		key.add("J.K. Rowling");
+
+
+		Book book2 =  new Book("Lord of the Rings: The Fellowship of the Ring", "J.K. Rowling", 2);
+        libray.addBook(book2);
+		ArrayList<Book> books = libray.searchBooks(SearchByType.AUTHOR, key);
+		assertEquals(2, books.size());
+	
+
+		libray.lendBook(book1, student);
+		books = libray.searchBooks(SearchByType.TITLE, key);
+		assertEquals(1, books.size());
+		assertEquals(book2, books.get(0));	
+	}
+
+	@Test
+	public void testSearchBooks_handle_duplicate_id() {
+		Library libray = new Library();
+		Student student = new Student("John Doe", 1);
+		libray.addStudent(student);
+		Book book1 = new Book("Harry Potter: The Philosopher's Stone", "J.K. Rowling", 1);
+        libray.addBook(book1);
+
+		ArrayList<Object> key = new ArrayList<Object>();
+		key.add(1);
+
+
+		Book book2 =  new Book("Lord of the Rings: The Fellowship of the Ring", "J.R.R. Tolkien", 1);
+		ArrayList<Book> books = libray.searchBooks(SearchByType.ID, key);
 		assertEquals(2, books.size());
 	
 
