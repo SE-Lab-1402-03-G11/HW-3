a8ff1da7e39d3012a975a66c4d78271fff394e64
mahdi <mahdi13812002@gmail.com>
2024-07-29 12:37:11 +0330

    add searchBooks test

---
+++ src/test/classes/LibraryTest.java
@@ -0,0 +1,130 @@
+package test.classes;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import main.classes.*;
+
+public class LibraryTest {
+
+	@Test
+	public void testSearchStudents_return_null() throws Exception{
+
+
+	}
+	
+	@Test
+	public void testSearchBooks_return_null1() {
+		Library libray = new Library();
+        libray.addBook(new Book("Harry Potter: The Philosopher's Stone", "J.K. Rowling", 1));
+        libray.addBook(new Book("Harry Potter: The Chamber of Secrets", "J.K. Rowling", 2));
+        libray.addBook(new Book("Harry Potter: The Prisoner of Azkaban", "J.K. Rowling", 3));
+        libray.addBook(new Book("Harry Potter: The Goblet of Fire", "J.K. Rowling", 4));
+        libray.addBook(new Book("Harry Potter: The Order of the Phoenix", "J.K. Rowling", 5));
+        libray.addBook(new Book("Harry Potter: The Half-Blood Prince", "J.K. Rowling", 6));
+        libray.addBook(new Book("Harry Potter: The Deathly Hallows", "J.K. Rowling", 7));
+
+		ArrayList<Object> key_title = new ArrayList<Object>();
+		key_title.add("Harry Potter");
+		ArrayList<Object> key_author = new ArrayList<Object>();
+		key_author.add("J.K.");
+		ArrayList<Object> key_id = new ArrayList<Object>();
+		key_id.add(0);
+
+		ArrayList<Book> books = libray.searchBooks(SearchByType.TITLE, key_title);
+		assertEquals(null, books);
+
+		books = libray.searchBooks(SearchByType.AUTHOR, key_author);
+		assertEquals(null, books);
+
+		books = libray.searchBooks(SearchByType.ID, key_id);
+		assertEquals(null, books);
+	}
+
+	@Test
+	public void testSearchBooks_search_by_title() {
+		Library libray = new Library();
+		Student student = new Student("John Doe", 1);
+		libray.addStudent(student);
+		Book book1 = new Book("Harry Potter: The Philosopher's Stone", "J.K. Rowling", 1);
+        libray.addBook(book1);
+
+		ArrayList<Object> key = new ArrayList<Object>();
+		key.add("Harry Potter: The Philosopher's Stone");
+		ArrayList<Book> books = libray.searchBooks(SearchByType.TITLE, key);
+		assertEquals(1, books.size());
+		assertEquals(book1, books.get(0));
+
+
+		Book book2 =  new Book("Harry Potter: The Chamber of Secrets", "J.K. Rowling", 2);
+        libray.addBook(book2);
+		key.add("Harry Potter: The Chamber of Secrets");
+		books = libray.searchBooks(SearchByType.TITLE, key);
+		assertEquals(2, books.size());
+	
+
+		libray.lendBook(book1, student);
+		books = libray.searchBooks(SearchByType.TITLE, key);
+		assertEquals(1, books.size());
+		assertEquals(book2, books.get(0));	
+	}
+
+	@Test
+	public void testSearchBooks_search_by_author() {
+		Library libray = new Library();
+		Student student = new Student("John Doe", 1);
+		libray.addStudent(student);
+		Book book1 = new Book("Harry Potter: The Philosopher's Stone", "J.K. Rowling", 1);
+        libray.addBook(book1);
+
+		ArrayList<Object> key = new ArrayList<Object>();
+		key.add("J.K. Rowling");
+		ArrayList<Book> books = libray.searchBooks(SearchByType.AUTHOR, key);
+		assertEquals(1, books.size());
+		assertEquals(book1, books.get(0));
+
+
+		Book book2 =  new Book("Lord of the Rings: The Fellowship of the Ring", "J.R.R. Tolkien", 2);
+        libray.addBook(book2);
+		key.add("J.R.R. Tolkien");
+		books = libray.searchBooks(SearchByType.AUTHOR, key);
+		assertEquals(2, books.size());
+	
+
+		libray.lendBook(book1, student);
+		books = libray.searchBooks(SearchByType.TITLE, key);
+		assertEquals(1, books.size());
+		assertEquals(book2, books.get(0));	
+	}
+
+	@Test
+	public void testSearchBooks_search_by_id() {
+		Library libray = new Library();
+		Student student = new Student("John Doe", 1);
+		libray.addStudent(student);
+		Book book1 = new Book("Harry Potter: The Philosopher's Stone", "J.K. Rowling", 1);
+        libray.addBook(book1);
+
+		ArrayList<Object> key = new ArrayList<Object>();
+		key.add(1);
+		ArrayList<Book> books = libray.searchBooks(SearchByType.ID, key);
+		assertEquals(1, books.size());
+		assertEquals(book1, books.get(0));
+
+
+		Book book2 =  new Book("Lord of the Rings: The Fellowship of the Ring", "J.R.R. Tolkien", 2);
+        libray.addBook(book2);
+		key.add(2);
+		books = libray.searchBooks(SearchByType.ID, key);
+		assertEquals(2, books.size());
+	
+
+		libray.lendBook(book1, student);
+		books = libray.searchBooks(SearchByType.ID, key);
+		assertEquals(1, books.size());
+		assertEquals(book2, books.get(0));	
+	}
+}
