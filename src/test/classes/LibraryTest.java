4bfda8626aeccfd49276c62c6bcc45d6c44e634c
Ali Rahmizad <79265219+AliAgha888@users.noreply.github.com>
2024-07-29 23:47:27 +0330

    Bring back part 1 tests tp LibraryTest.java

--- src/test/classes/LibraryTest.java
+++ src/test/classes/LibraryTest.java
@@ -1,6 +1,7 @@
 package test.classes;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -10,6 +11,34 @@ import org.junit.Test;
 import main.classes.*;
 
 public class LibraryTest {
+
+	@Test
+	public void testStudentShouldNotHaveTheBookAfterReturningIt() {
+		Library library = new Library();
+
+		Book book1 = new Book("Book-1", "Author-1", 10);
+		library.addBook(book1);
+
+		Student student1 = new Student("Alice", 10);
+		library.addStudent(student1);
+
+		library.lendBook(book1, student1);
+		library.returnBook(book1, student1);
+
+		assertFalse(student1.hasBook(book1));
+	}
+
+	@Test
+	public void testStudentShouldRegisterBeforeLendingABook() {
+		Library library = new Library();
+
+		Book book1 = new Book("Book-1", "Author-1", 10);
+		library.addBook(book1);
+
+		Student student1 = new Student("Alice", 10);
+
+		assertFalse(library.lendBook(book1, student1));
+	}
 
 	@Test
 	public void testSearchStudents_return_null() throws Exception{
